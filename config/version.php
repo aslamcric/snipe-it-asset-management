de0f6b43e3e72a32807f77bc7e2bb1af6a6fb8fe
aslamcric <mdaslamcric@gmail.com>
2025-12-09 17:36:21 +0600

    Initialized, Header, Footer, Navbar

---
+++ config/version.php
@@ -0,0 +1,10 @@
+<?php
+return array (
+  'app_version' => 'v8.3.6',
+  'full_app_version' => 'v8.3.6 - build 20569-g9e23117f9',
+  'build_version' => '20569',
+  'prerelease_version' => '',
+  'hash_version' => 'g9e23117f9',
+  'full_hash' => 'v8.3.6-16-g9e23117f9',
+  'branch' => 'master',
+);
